baafd24947f7b2d430ab0b6dd9ac2b002ed7be34
es1294 <johnbola@buffalo.edu>
2019-03-28 18:55:19 -0400

    completed foward pass and output deltas

--- nnFunctions.py
+++ nnFunctions.py
@@ -63,48 +63,44 @@ def sigmoid(z):
     #if its a scalar then just do sigmoid activation
     #else iterate through the matrix and do sigmoid activation on each element and store them in s
     if np.isscalar(z):
-        s = 1 / (1 + exp(-1 * z))
+        s = 1 / (1 + np.exp(-1 * z))
     else:
         s = np.zeros(z.shape)
         for ix, iy in np.ndindex(z.shape):
             temp = z[ix,iy]
-            sigmoid_result = 1 / (1 + exp(-1 * temp))
-            print(str(sigmoid_result))
+            sigmoid_result = 1 / (1 + np.exp(-1 * temp))
             s[ix,iy] = sigmoid_result
-
-
-
     return s
 
 def nnObjFunction(params, *args):
     '''
-    % nnObjFunction computes the value of objective function (cross-entropy
-    % with regularization) given the weights and the training data and lambda
-    % - regularization hyper-parameter.
+        % nnObjFunction computes the value of objective function (cross-entropy
+        % with regularization) given the weights and the training data and lambda
+        % - regularization hyper-parameter.
 
-    % Input:
-    % params: vector of weights of 2 matrices W1 (weights of connections from
-    %     input layer to hidden layer) and W2 (weights of connections from
-    %     hidden layer to output layer) where all of the weights are contained
-    %     in a single vector.
-    % n_input: number of node in input layer (not including the bias node)
-    % n_hidden: number of node in hidden layer (not including the bias node)
-    % n_class: number of node in output layer (number of classes in
-    %     classification problem
-    % train_data: matrix of training data. Each row of this matrix
-    %     represents the feature vector of a particular image
-    % train_label: the vector of truth label of training images. Each entry
-    %     in the vector represents the truth label of its corresponding image.
-    % lambda: regularization hyper-parameter. This value is used for fixing the
-    %     overfitting problem.
+        % Input:
+        % params: vector of weights of 2 matrices W1 (weights of connections from
+        %     input layer to hidden layer) and W2 (weights of connections from
+        %     hidden layer to output layer) where all of the weights are contained
+        %     in a single vector.
+        % n_input: number of node in input layer (not including the bias node)
+        % n_hidden: number of node in hidden layer (not including the bias node)
+        % n_class: number of node in output layer (number of classes in
+        %     classification problem
+        % train_data: matrix of training data. Each row of this matrix
+        %     represents the feature vector of a particular image
+        % train_label: the vector of truth label of training images. Each entry
+        %     in the vector represents the truth label of its corresponding image.
+        % lambda: regularization hyper-parameter. This value is used for fixing the
+        %     overfitting problem.
 
-    % Output:
-    % obj_val: a scalar value representing value of error function
-    % obj_grad: a SINGLE vector (not a matrix) of gradient value of error function
-    % NOTE: how to compute obj_grad
-    % Use backpropagation algorithm to compute the gradient of error function
-    % for each weights in weight matrices.
-    '''
+        % Output:
+        % obj_val: a scalar value representing value of error function
+        % obj_grad: a SINGLE vector (not a matrix) of gradient value of error function
+        % NOTE: how to compute obj_grad
+        % Use backpropagation algorithm to compute the gradient of error function
+        % for each weights in weight matrices.
+        '''
     n_input, n_hidden, n_class, train_data, train_label, lambdaval = args
     # First reshape 'params' vector into 2 matrices of weights W1 and W2
 
@@ -112,14 +108,67 @@ def nnObjFunction(params, *args):
     W2 = params[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
     obj_val = 0
 
-    # Your code here
-    #
-    #
-    #
-    #
-    #
+    # insert bias into training data
+    train_data = np.insert(train_data, 0, 1, axis=1)
 
+    # number of inputs that the network will compute outputs to
+    N = train_data.shape[0]
 
+    # stores the outpult result for each output node
+    output_result = np.zeros(n_class)
+
+    # outerloop is what will do foward pass and back propogation
+    for i in range(N):
+
+        # is the current input into the network
+        xi = train_data[i]
+
+        # stores the delta for each output unit
+        deltas_output = np.zeros(n_class)
+
+        # stores the delta for each hidden unit
+        deltas_hidden = np.zeros(n_hidden)
+
+        # stores the result of the hidden layer at each node
+        z_hidden_result = np.zeros(n_hidden)
+
+        # computes foward pass from input to hidden
+        for j in range(n_hidden):
+            # the weights for the given hidden node
+            W1_i = W1[j]
+
+            # computes the dot product of the input and the weights for hidden node
+            W1_mul_xi = np.dot(W1_i, xi)
+
+            # computes the sigmoid activation for the hidden node.
+            sigmoid_activation = sigmoid(W1_mul_xi)
+
+            # stores the result of the sigmoid activation to its corresponding hidden node unit
+            z_hidden_result[j] = sigmoid_activation
+
+        # inserts the bias at the hidden layer
+        z_hidden_result = np.insert(z_hidden_result, 0, 1)
+
+        # starts the foward propogation from hidden layer to output layer
+        # same algorithim as above
+        for j in range(n_class):
+            W2_i = W2[j]
+            W2_mul_hiddeni = np.dot(W2_i, z_hidden_result)
+
+            sigmoid_activation = sigmoid(W2_mul_hiddeni)
+
+            output_result[j] = sigmoid_activation
+
+        # computes the loss function of the outputs to the true label
+        # loss = np.subtract(train_label, output_result) ** 2
+        # loss = np.sum(loss) / 2
+
+        # compute delta for the output
+        for i in range(n_class):
+            output_i = output_result[i]
+            truelabel_yi = train_label[i]
+            delta = output_i * (1 - output_i) * (truelabel_yi - output_i)
+            deltas_output[i] = delta
 
     # Make sure you reshape the gradient matrices to a 1D array. for instance if
     # your gradient matrices are grad_W1 and grad_W2
--- nnScript.py
+++ nnScript.py
@@ -49,5 +49,16 @@ print('\n Training set Accuracy:' + str(100 * np.mean((predicted_label == train_
 predicted_label = nnPredict(W1, W2, test_data)
 print('\n Test set Accuracy:    ' + str(100 * np.mean((predicted_label == test_label).astype(float))) + '%')
 
-a = np.arange(0,60,5)
-a = a.reshape(3,4)
+n_input = 5
+n_hidden = 3
+n_class = 2
+training_data = np.array([np.linspace(0,1,num=5),np.linspace(1,0,num=5)])
+training_label = np.array([0,1])
+lambdaval = 0
+params = np.linspace(-5,5, num=26)
+args = (n_input, n_hidden, n_class, training_data, training_label, lambdaval)
+objval,objgrad = nnObjFunction(params, *args)
+print("Objective value:")
+print(objval)
+print("Gradient values: ")
+print(objgrad)
