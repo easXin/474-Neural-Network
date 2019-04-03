b54686a8c577cffe06ff81d29238eae7ed534300
Jackie Ling <jling2@buffalo.edu>
2019-04-02 21:30:47 -0400

    Update deepnnScript.py

--- deepnnScript.py
+++ deepnnScript.py
@@ -125,10 +125,10 @@ def multilayer_perceptron(x):
     # layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     # layer_2 = tf.nn.sigmoid(layer_2)
 
-    # layer_3 = tf.add(tf.matmul(x, weights['h3']), biases['b3'])
+    # layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
     # layer_3 = tf.nn.sigmoid(layer_3)
 
-    # layer_4 = tf.add(tf.matmul(layer_1, weights['h4']), biases['b4'])
+    # layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
     # layer_4 = tf.nn.sigmoid(layer_4)
 
     # out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
@@ -142,16 +142,16 @@ def multilayer_perceptron(x):
     # layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     # layer_2 = tf.nn.sigmoid(layer_2)
 
-    # layer_3 = tf.add(tf.matmul(x, weights['h3']), biases['b3'])
+    # layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
     # layer_3 = tf.nn.sigmoid(layer_3)
 
-    # layer_4 = tf.add(tf.matmul(layer_1, weights['h4']), biases['b4'])
+    # layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
     # layer_4 = tf.nn.sigmoid(layer_4)
 
-    # layer_5 = tf.add(tf.matmul(x, weights['h5']), biases['b5'])
+    # layer_5 = tf.add(tf.matmul(layer_4, weights['h5']), biases['b5'])
     # layer_5 = tf.nn.sigmoid(layer_5)
 
-    # layer_6 = tf.add(tf.matmul(layer_1, weights['h6']), biases['b6'])
+    # layer_6 = tf.add(tf.matmul(layer_5, weights['h6']), biases['b6'])
     # layer_6 = tf.nn.sigmoid(layer_6)
 
     # out_layer = tf.matmul(layer_6, weights['out']) + biases['out']
